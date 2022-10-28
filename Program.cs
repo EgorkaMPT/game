8aacee2cb3d1e8fd40f1177b4dfaa21f17aed3d9
EgorkaMPT <112894120+EgorkaMPT@users.noreply.github.com>
2022-10-28 16:56:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿namespace game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Угадай число \n2.Таблица умножений \n3.Вывод делителей числа");
+            Console.Write("Выберите действие: ");
+            int a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Game();
+                    Main(args);
+                break;
+                case 2:
+                    Tables();
+                    Main(args);
+                    break;
+                case 3:
+                    Divs();
+                    Main(args);
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели некоректное действие");
+                    Main(args);
+                    break;
+            }
+
+        }
+
+        private static void Game()
+        {
+            Random random = new Random();
+            int rnum = 0;
+            rnum = random.Next(0,101);
+            Console.WriteLine("Введите число");
+            int ynum = int.Parse(Console.ReadLine());
+            while (rnum != ynum)
+            {
+                if (ynum > rnum)
+                {
+                    Console.WriteLine("Введите число меньше");
+                }
+                if (ynum < rnum)
+                {
+                    Console.WriteLine("Введите число больше");
+                }
+            ynum = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Поздравляю, вы выиграли");
+        }
+
+        private static void Tables()
+        {
+            int[,] table = new int[10, 10];
+            int tab = 2;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    table[i, j] = j * i; 
+                }
+                
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write(table[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Divs()
+        {
+            int ynum = int.Parse(Console.ReadLine());
+            for (int i = 1; i < ynum; i++)
+            {
+                if (ynum % i == 0)
+                {
+                    Console.Write("\t" + i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
